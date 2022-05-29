0a31aaabed11c38b07250d2aa8d4ebbba490a7c6
OlenaHanich <olenahanich@gmail.com>
2022-05-29 16:04:12 +0300

    Lab 1

---
+++ Labs/Lab1_OlenaHanich.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+# # **1. Markdown cells**
+# * Hi! Its Main Academy?
+# * All HTML tabs are supported
+# * Моя сторінка на лінкедін [Лінкедін](linkedin.com)
+# 
+# # This is Heading1
+# ## This is Heading2
+# 
+# some gray colored text:
+# > This is where we can bring quotes and present them nicely
+# 
+# And this is the paragraph about something very important. Even maths:  
+# $$
+#     y_{sub}=\sqrt(x_{sub})
+# $$
+# Or Python code example:
+# <font size = 2>  
+# &nbsp;&nbsp;import math  
+# &nbsp;&nbsp;def foo():  
+# &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;return "foo"    
+# </font>
+# 
+# Here is the sample mean:  
+# $$
+#     \bar y = \frac{1}{n} \sum _{i=1}^{n}y_i
+# $$
+# 
+# Here is the sample variance:  
+# $$ 
+#     \sigma^2= \frac{\displaystyle \sum _{i=1}^{n}(y_i-\bar y)^2}{n-1}
+# $$
+# 
+# Here is the sample standart deviation:  
+# $$ 
+#     \sigma= \sqrt {\frac{\displaystyle \sum _{i=1}^{n}(y_i-\bar y)^2}{n-1}}
+# $$
+
+
